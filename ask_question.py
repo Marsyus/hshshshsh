ab453451fb42e4319af4204b625a7fd50d4f565d
Marsyus <miadolomboy0917@gmail.com>
2025-06-21 14:26:12 +0800

    Ask Eunice if i can see the schedules for our departmental exam

---
+++ ask_question.py
@@ -0,0 +1,6 @@
+class Ask:
+    def __init__(self):
+        self.question = "pre pwede patingin ng sched sa deptals?"
+
+    def ask(self):
+        print(self.question)
---
+++ main.py
@@ -0,0 +1,4 @@
+from ask_question import Ask
+
+main = Ask()
+main.ask()
